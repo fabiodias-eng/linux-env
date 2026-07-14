6900e129cb27468662727ab7077454ed64be851d
FabioDias <dias.c.fabio@gmail.com>
2026-07-14 01:28:48 -0300

    feat: change installation config process

--- setup/configure.sh
+++ setup/configure.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+set -e
 
 # Theme set
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
@@ -5,5 +8,11 @@ gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
 # Enable Laptop Touch Click
 xinput set-prop 11 "libinput Tapping Enabled" 1
 
-
-
+dotfiles="$(pwd)/dotfiles "
+cp -r "$dotfiles/.config $HOME"
+cp -r "$dotfiles/.librewolf $HOME"
+cp -r "$dotfiles/.local $HOME"
+cp "$dotfiles/.Xresources $HOME"
+cp "$dotfiles/.bashrc $HOME"
+cp "$dotfiles/.profile $HOME"
+cp "$dotfiles/.xinitrc $HOME"
