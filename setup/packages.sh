05d7bee334da205f15aac43b1fbf946a17f82fd8
Fabio Dias <137723212+fabiodias-eng@users.noreply.github.com>
2026-09-13 20:29:49 -0300

    fix: adjust ubuntu-drivers package naming to ubuntu-drivers-common

--- setup/packages.sh
+++ setup/packages.sh
@@ -45,7 +45,7 @@ xlib_packages() {
 
 graphic_packages() {
     sudo apt install -y \
-        ubuntu-drivers \
+        ubuntu-drivers-common \
         mesa-utils \
         mesa-va-drivers \
         mesa-vdpau-drivers \
